64f90e0bb99f8e6073507338d4dd518598573e12
nehamdr225 <nehamdr225@gmail.com>
2019-08-09 19:41:38 +0545

    changes

--- clinet.py
+++ clinet.py
@@ -6,8 +6,8 @@ BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect()
-s.sendto(MESSAGE.encode(), (TCP_IP, TCP_PORT))
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE.encode())
 data = s.recv(BUFFER_SIZE)
 s.close()
 
